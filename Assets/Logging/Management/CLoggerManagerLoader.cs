2bf195036c0279166d858543e7d606519effe2dc
Fabricio Caneva <hardlymersk@gmail.com>
2019-07-28 16:07:17 -0300

    - Removed unused files

--- Assets/Logging/Management/CLoggerManagerLoader.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace caneva20.Logging.Management {
-    public class CLoggerManagerLoader : MonoBehaviour {
-        private void Awake() {
-            LoggerManager.Initialize();
-        }
-    }
-}
