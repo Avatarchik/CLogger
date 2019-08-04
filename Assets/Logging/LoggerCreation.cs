70d6ecb08fcd51892442dee1b1742461993112e8
Fabricio Caneva <hardlymersk@gmail.com>
2019-08-04 12:50:32 -0300

    Follow up from the previous commit

---
+++ Assets/Logging/LoggerCreation.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using caneva20.Logging.Loggers;
+using caneva20.Logging.Management.Configurations;
+
+namespace caneva20.Logging {
+    public static partial class Logger {
+        private const int FRAME_INDEX = 3;
+        private static readonly Dictionary<Type, ICLogger> _loggers = new Dictionary<Type, ICLogger>();
+
+        private static Type GetDeclaringType() {
+            return new StackTrace().GetFrame(FRAME_INDEX).GetMethod().DeclaringType;
+        }
+
+        private static ICLogger Get() {
+            var type = GetDeclaringType();
+
+            if (_loggers.ContainsKey(type)) {
+                return _loggers[type];
+            }
+
+            var logger = Create(ManagerConfig.Instance[type]);
+            
+            _loggers.Add(type, logger);
+
+            return logger;
+        }
+        
+        private static ICLogger Create(LoggerConfig config) {
+        #if UNITY_EDITOR
+            return new EditorLogger(config);
+        #else
+            return new NativeLogger(config);
+        #endif
+        }
+    }
+}
